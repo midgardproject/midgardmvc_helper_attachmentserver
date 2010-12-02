4c82028597ea71dc8d93e652995e7b9ae4d7b502
Henri Bergius <henri.bergius@iki.fi>
2010-12-02 13:32:53 +0200

    Add a default config

--- injector.php
+++ injector.php
@@ -4,7 +4,7 @@ class midgardmvc_helper_attachmentserver_injector
     public function inject_process(midgardmvc_core_request $request)
     {
         // We inject the process to provide our additional root routes
-        $request->add_component_to_chain(midgardmvc_core::get_instance()->component->get('midgardmvc_helper_attachmentserver'), true);
+        $request->add_component_to_chain(midgardmvc_core::get_instance()->component->get('midgardmvc_helper_attachmentserver'), false);
     }
 }
 ?>
